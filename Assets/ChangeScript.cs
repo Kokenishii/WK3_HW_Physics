beefa1c5e2aecb4ac0e81b2d6c62921045f08893
Unknown <ki575@nyu.edu>
2017-09-26 10:40:27 -0400

    Initial Commit

---
+++ Assets/ChangeScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeScript : MonoBehaviour {
+    public Transform newLookAt;
+    public Transform newMoveTo;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter(Collider player)
+    {
+        if(player.tag == "Player")
+        {
+            CameraScript myCamera = Camera.main.GetComponent<CameraScript>();
+           if(newLookAt!=null) myCamera.lookAt = newLookAt;
+            if (newMoveTo != null) myCamera.moveTo = newMoveTo;
+        }
+    }
+}
---
+++ Assets/Generator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode]
+public class Generator : MonoBehaviour {
+  
+    public GameObject myObject;
+    public float dominoNumber; 
+    // Use this for initialization
+    void Start () {
+		for(int i = 0;i<dominoNumber;i++)
+        {
+            GameObject newObject = Instantiate(myObject, new Vector3(0, 0, 0),Quaternion.Euler(0,i*360/dominoNumber,0));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
